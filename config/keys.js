ab723a654a65ba7291494f566dfd64d6da8c36fe
eeefg650 <rodadin96@gmail.com>
2021-06-20 03:13:54 +0300

    תיקון באג התחברות לDB

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,6 @@
 dbPassword =
   "mongodb+srv://haim40:dinroda123@cluster-fortnite.kw9pn.mongodb.net/Points&Users";
 
-module.exports = {
-  mongoURI: dbPassword,
-  session: {
-    cookieKey: "imdinrodaimthebest",
-  },
-};
+module.exports = dbPassword;
 
 // EgC#-StMY.8px.L
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const db = require("./config/keys").mongoURI;
+const db = require("./config/keys");
 const helmet = require("helmet");
 
 // Connect to MongoDB
